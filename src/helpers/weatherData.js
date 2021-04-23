d79a3e510c6df740b66b6e0e7f9c52ec2de3bd6c
fimoefive <brothermj@hotmail.com>
2021-04-22 22:11:42 -0500

    setup getWeather function and setup apiKey in .env file.

--- src/helpers/weatherData.js
+++ src/helpers/weatherData.js
@@ -1,2 +1,10 @@
 import axios from 'axios';
-import firebaseConfig from '../helpers/apiKeys';
+import firebaseConfig from './apiKeys';
+
+const getWeather = (city) => new Promise((resolve, reject) => {
+  axios.get(`https://api.openweathermap.org/data/2.5/onecall?q=${city}4&exclude=hourly,daily&appid=${firebaseConfig.apiKey}&units=imperial`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
+
+export default getWeather;
